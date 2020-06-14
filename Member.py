e76412dcaa46829fcf9cf1482e007fb67ab8ec98
ZHChen8397 <ZHChen8397@gmail.com>
2020-06-14 16:14:19 +0800

    implement Member.py

---
+++ Member.py
@@ -0,0 +1,7 @@
+class Member:
+    def __init__(self, name, gender, amount= 0, record= [], level= 'bronze'):
+        self.name = name
+        self.gender = gender
+        self.amount = amount
+        self.record = record
+        self.level = level
